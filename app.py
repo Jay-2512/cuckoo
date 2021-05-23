7e0f630db9c3ab181259cef33645975b9ed025d4
Jayasurya J <jayasooryapj@gmail.com>
2021-05-23 13:33:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ path = os.getcwd()
 
 app = create_app(path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))
