c1d2c9d7a29879cdeb48596af137f7a5cf309a76
Jayasurya J <jayasooryapj@gmail.com>
2021-05-23 12:55:31 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from logging import debug
-from website import create_app
-from website.twitter_login import twitter_login
-
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
